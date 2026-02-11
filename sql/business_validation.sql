ffaad4e4b7dfbeb0f7e548c84232f96bfb37d386
Luyanda Zuma <luyanda.s.zuma@gmail.com>
2026-02-11 13:37:08 +0200

    Fix table names in business_validation.sql

--- sql/business_validation.sql
+++ sql/business_validation.sql
@@ -5,10 +5,10 @@ WHERE quantity < 0 OR unitprice < 0;
 
 --Query 2: Check for orders with future dates (impossible)
 SELECT COUNT(*) as future_orders
-FROM "retail_db"."clean_processed"
+FROM "retail-db"."clean-processed"
 WHERE invoicedate > current_date;
 
 --Query 3: Check for transactions with unknown countries
 SELECT COUNT(*) as unknown_country_count
-FROM "retail_db"."clean_processed"
+FROM "retail-db"."clean-processed"
 WHERE country IS NULL OR country = '';
